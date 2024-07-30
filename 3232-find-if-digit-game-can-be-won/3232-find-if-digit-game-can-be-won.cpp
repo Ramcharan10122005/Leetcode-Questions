10495b526cafe86bb335896bfeeeaf2039c19264
Ramcharan Kundena <160163532+Ramcharan10122005@users.noreply.github.com>
2024-07-30 12:40:41 +0530

    Time: 3 ms (91.08%), Space: 27.9 MB (95.05%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int sing=0,doub=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=0&&nums[i]<=9)
+            sing+=nums[i];
+            else
+            doub+=nums[i];
+        }
+        return sing!=doub;
+    }
+};
