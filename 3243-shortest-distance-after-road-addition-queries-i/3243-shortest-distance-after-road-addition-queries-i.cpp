515ba55755eccae095b53dca8b89b26201a45a16
Ramcharan Kundena <160163532+Ramcharan10122005@users.noreply.github.com>
2024-08-05 14:40:32 +0530

    Time: 477 ms (14.29%), Space: 129.7 MB (14.29%) - LeetHub

---
+++ 3243-shortest-distance-after-road-addition-queries-i/3243-shortest-distance-after-road-addition-queries-i.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <queue>
+#include <limits>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (int i = 0; i < n - 1; ++i) {
+            adj[i].emplace_back(i + 1, 1); 
+        }
+
+        vector<int> result;
+        auto dijkstra = [&](int start, int target) {
+            vector<int> dist(n, INT_MAX);
+            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+            pq.emplace(0, start);
+            dist[start] = 0;
+
+            while (!pq.empty()) {
+                int u = pq.top().second;
+                int d = pq.top().first;
+                pq.pop();
+
+                if (d > dist[u]) continue;
+
+                for (auto& edge : adj[u]) {
+                    int v = edge.first;
+                    int weight = edge.second;
+                    if (dist[u] + weight < dist[v]) {
+                        dist[v] = dist[u] + weight;
+                        pq.emplace(dist[v], v);
+                    }
+                }
+            }
+
+            return dist[target] == INT_MAX ? -1 : dist[target];
+        };
+
+        for (const auto& q : queries) {
+            int u = q[0], v = q[1];
+            adj[u].emplace_back(v, 1); 
+            int shortest = dijkstra(0, n - 1);
+            result.push_back(shortest);
+        }
+
+        return result;
+    }
+};
