a7dbdd5776c3b1c2b69a95cf38f4de21e745c250
Ramcharan Kundena <160163532+Ramcharan10122005@users.noreply.github.com>
2024-09-02 20:20:31 +0530

    Time: 70 ms (78.67%), Space: 58.3 MB (81.6%) - LeetHub

--- 1537-get-the-maximum-score/1537-get-the-maximum-score.cpp
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     int maxSum(vector<int>& arr1, vector<int>& arr2) {
-        int i=0,j=0,sum1=0,sum2=0,ans=0;
-        while(i<arr1.size()&&j<arr2.size()){
-            if(arr1[i]<arr2[j]){
-                sum1+=arr1[i];
+        long long i = 0, j = 0;
+        long long sum1 = 0, sum2 = 0, ans = 0;
+        long long MOD = 1e9 + 7; 
+        while (i < arr1.size() && j < arr2.size()) {
+            if (arr1[i] < arr2[j]) {
+                sum1 += arr1[i];
                 i++;
-            }
-            else if(arr1[i]>arr2[j]){
-                sum2+=arr2[j];
+            } else if (arr1[i] > arr2[j]) {
+                sum2 += arr2[j];
                 j++;
-            }
-            else{
-                ans+=max(sum1,sum2);
-                ans+=arr1[i];
+            } else {
+                ans += max(sum1, sum2);
+                ans += arr1[i];  
+                ans %= MOD;  
                 i++;
                 j++;
-                sum1=0,sum2=0;
+                sum1 = 0;
+                sum2 = 0;
             }
         }
-        while(i<arr1.size()){
-            sum1+=arr1[i];
+
+        while (i < arr1.size()) {
+            sum1 += arr1[i];
             i++;
         }
-        while(j<arr2.size()){
-            sum2+=arr2[j];
+        while (j < arr2.size()) {
+            sum2 += arr2[j];
             j++;
         }
-        ans+=max(sum1,sum2);
-        return ans;
+
+        ans += max(sum1, sum2);
+        ans %= MOD;  
+        return ans % MOD;  
     }
 };
